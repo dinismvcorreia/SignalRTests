845c3e5881579ededb9f0beb6bbf8037d3814e75
Dinis Correia <dinis_prt@hotmail.com>
2016-09-21 21:11:50 +0100

    Create Hub client and simple communication

--- Hub/TestHub.cs
+++ Hub/TestHub.cs
@@ -1,10 +1,17 @@
 ﻿namespace Hub
 {
+    using System;
     using Microsoft.AspNet.SignalR;
     using Microsoft.AspNet.SignalR.Hubs;
 
     [HubName("TestHub")]
     public class TestHub : Hub
     {
+        public void DetermineLength(string message)
+        {
+            Console.WriteLine(message);
+            var newMessage = string.Format($"{message} has a length of: {message.Length}");
+            Clients.All.ReceiveLength(newMessage);
+        }
     }
 }
---
+++ HubClient/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HubClient
+{
+    using System;
+    using Microsoft.AspNet.SignalR.Client;
+
+    public class Program
+    {
+        static void Main()
+        {
+            const string url = @"http://localhost:8080/";
+
+            var connection = new HubConnection(url);
+            var hub = connection.CreateHubProxy("TestHub");
+            connection.Start().Wait();
+
+            hub.On("ReceiveLength", x => Console.WriteLine(x));
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                hub.Invoke("DetermineLength", line).Wait();
+            }
+        }
+    }
+}
